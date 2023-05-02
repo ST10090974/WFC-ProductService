5fa7f1a4d9617a43a746d2ba4b25d810217334aa
lab_services_student <ST10090974@vcconnect.edu.za>
2023-05-02 06:43:56 +0000

    Service Supplier

---
+++ ProductService/IServiceSupplier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ProductService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IServiceSupplier" in both code and config file together.
+    [ServiceContract]
+    public interface IServiceSupplier
+    {
+        [OperationContract]
+        void DoWork();
+    }
+}
